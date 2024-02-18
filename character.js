cb39c9d70b56513b2b63d85640dbee8345ea82d2
Graveyardcitizen <Maldeathenize@gmail.com>
2024-02-18 12:36:06 +0100

    testing

---
+++ character.js
@@ -0,0 +1,118 @@
+// Thanks barrel for helping with the syntax that is JS
+document.addEventListener("DOMContentLoaded", function () {
+    let textbox = document.getElementById("textbox");
+
+    const texts = {
+        Pechi: "Pechi",
+        Hilda: "Hilda",
+        Valerian: "Valerian",
+        Huhner: "HUH?",
+        Beatrice: "Beatrice",
+        Rav: "Rav",
+        Elliot: "Elliot",
+        Veri: "Veroni",
+        Diph: "Dip",    
+        May: "May",    
+        Bibi: "Bibi",    
+        Mara: "Mara",    
+        Diaz: "Diaz",    
+        Muertoz: "Muertoz",    
+        Sume: "Sume",    
+        Cleora: "Cleora",    
+        Ori: "Ori",    
+        Caramelo: "Caramelo",    
+        Bermuda: "Bermuda",    
+        Heizenga: "Heizenga",  
+        Boroque: "Boroque",    
+        Zal: "Zal",    
+        Fish: "Fish",    
+        NoChar: "???",
+    };
+
+    document.querySelectorAll('[id]').forEach((element) => {
+        element.addEventListener("mouseenter", (event) => {
+            const id = element.id;
+            textbox.innerHTML = texts[id] || "";
+        });
+
+        element.addEventListener("mouseleave", (event) => {
+            textbox.innerHTML = "";
+        });
+    });
+});
+
+function checkPassword() {
+    // Numbercode
+    var code = document.getElementById("codeInput").value;
+
+    var codeNumber = parseInt(code);
+
+    switch (codeNumber) {
+        case 0001:
+            window.location.href = "https://ghosticface.github.io/Skullmare/1-ch.html";
+            break;
+        case 0002:
+            window.location.href = "https://ghosticface.github.io/Skullmare/2-ch.html";
+            break;
+        case 0003:
+            window.location.href = "https://ghosticface.github.io/Skullmare/3-ch.html";
+            break;
+        case 0004:
+            window.location.href = "https://ghosticface.github.io/Skullmare/4-ch.html";
+            break;
+        case 0005:
+            window.location.href = "https://ghosticface.github.io/Skullmare/5-ch.html";
+            break;
+        case 0006:
+            window.location.href = "https://ghosticface.github.io/Skullmare/6-ch.html";
+            break;
+        case 0021:
+            window.location.href = "https://ghosticface.github.io/Skullmare/21-ch.html";
+            break;
+        case 0023:
+            window.location.href = "https://ghosticface.github.io/Skullmare/23-ch.html";
+            break;
+        case 0025:
+            window.location.href = "https://ghosticface.github.io/Skullmare/25-ch.html";
+            break;
+        case 0026:
+            window.location.href = "https://ghosticface.github.io/Skullmare/26-ch.html";
+            break;
+        case 0041:
+            window.location.href = "https://ghosticface.github.io/Skullmare/41-ch.html";
+            break;
+        case 0042:
+            window.location.href = "https://ghosticface.github.io/Skullmare/42-ch.html";
+            break;
+        case 0043:
+            window.location.href = "https://ghosticface.github.io/Skullmare/43-ch.html";
+            break;
+        case 0044:
+            window.location.href = "https://ghosticface.github.io/Skullmare/44-ch.html";
+            break;
+        case 0047:
+            window.location.href = "https://ghosticface.github.io/Skullmare/47-ch.html";
+            break;
+        case 0051:
+            window.location.href = "https://ghosticface.github.io/Skullmare/51-ch.html";
+            break;
+        case 0056:
+            window.location.href = "https://ghosticface.github.io/Skullmare/56-ch.html";
+            break;
+        case 0064:
+            window.location.href = "https://ghosticface.github.io/Skullmare/64-ch.html";
+            break;
+        case 0081:
+            window.location.href = "https://ghosticface.github.io/Skullmare/81-ch.html";
+            break;
+        case 0099:
+            window.location.href = "https://ghosticface.github.io/Skullmare/99-ch.html";
+            break;
+        case ▲★▲:
+            window.location.href = "https://ghosticface.github.io/Skullmare/99-ch.html";
+            break;
+
+        default:
+            alert("OOOPS = The characterLock is not available/Not yet been made");
+    }
+}
