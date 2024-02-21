387a4db7269c283c6361040bbabbd6d18d249c39
Graveyardcitizen <Maldeathenize@gmail.com>
2024-02-21 15:23:11 +0100

    commit

--- character.js
+++ character.js
@@ -49,67 +49,67 @@ function checkPassword() {
 
     switch (codeNumber) {
         case 0001:
-            window.location.href = "https://ghosticface.github.io/Skullmare/1-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/1-ch.html";
             break;
         case 0002:
-            window.location.href = "https://ghosticface.github.io/Skullmare/2-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/2-ch.html";
             break;
         case 0003:
-            window.location.href = "https://ghosticface.github.io/Skullmare/3-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/3-ch.html";
             break;
         case 0004:
-            window.location.href = "https://ghosticface.github.io/Skullmare/4-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/4-ch.html";
             break;
         case 0005:
-            window.location.href = "https://ghosticface.github.io/Skullmare/5-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/5-ch.html";
             break;
         case 0006:
-            window.location.href = "https://ghosticface.github.io/Skullmare/6-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/6-ch.html";
             break;
         case 0021:
-            window.location.href = "https://ghosticface.github.io/Skullmare/21-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/21-ch.html";
             break;
         case 0023:
-            window.location.href = "https://ghosticface.github.io/Skullmare/23-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/23-ch.html";
             break;
         case 0025:
-            window.location.href = "https://ghosticface.github.io/Skullmare/25-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/25-ch.html";
             break;
         case 0026:
-            window.location.href = "https://ghosticface.github.io/Skullmare/26-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/26-ch.html";
             break;
         case 0041:
-            window.location.href = "https://ghosticface.github.io/Skullmare/41-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/41-ch.html";
             break;
         case 0042:
-            window.location.href = "https://ghosticface.github.io/Skullmare/42-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/42-ch.html";
             break;
         case 0043:
-            window.location.href = "https://ghosticface.github.io/Skullmare/43-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/43-ch.html";
             break;
         case 0044:
-            window.location.href = "https://ghosticface.github.io/Skullmare/44-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/44-ch.html";
             break;
         case 0047:
-            window.location.href = "https://ghosticface.github.io/Skullmare/47-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/47-ch.html";
             break;
         case 0051:
-            window.location.href = "https://ghosticface.github.io/Skullmare/51-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/51-ch.html";
             break;
         case 0056:
-            window.location.href = "https://ghosticface.github.io/Skullmare/56-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/56-ch.html";
             break;
         case 0064:
-            window.location.href = "https://ghosticface.github.io/Skullmare/64-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/64-ch.html";
             break;
         case 0081:
-            window.location.href = "https://ghosticface.github.io/Skullmare/81-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/81-ch.html";
             break;
         case 0099:
-            window.location.href = "https://ghosticface.github.io/Skullmare/99-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/99-ch.html";
             break;
         case ▲★▲:
-            window.location.href = "https://ghosticface.github.io/Skullmare/99-ch.html";
+            window.location.href = "https://deathenize.github.io/Deathenize/99-ch.html";
             break;
 
         default:
